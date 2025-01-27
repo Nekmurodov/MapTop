dc980c99935b98601833463066f4456de7d9d31d
@Nekmurodov <nekmurodovdilshodbek@gmail.com>
2025-01-27 15:40:40 +0500

    bismillah

---
+++ src/main/java/com/example/MapTop/MapTopApplication.java
@@ -0,0 +1,13 @@
+package com.example.MapTop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapTopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MapTopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/MapTop/MapTopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MapTop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MapTopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
